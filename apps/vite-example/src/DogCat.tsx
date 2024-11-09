f4c6674ef918b9ceca9c7096a2e13ec8b63f3af0
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2024-11-09 14:46:38 -0800

    cat dog test

---
+++ apps/vite-example/src/DogCat.tsx
@@ -0,0 +1,8 @@
+export default function DogCat() {
+  return (
+    <div>
+      <img src="https://picsum.photos/id/237/200/300" />
+      <img src="https://picsum.photos/id/324/200/300" />
+    </div>
+  );
+}
