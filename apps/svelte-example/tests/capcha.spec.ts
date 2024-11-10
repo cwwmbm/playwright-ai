bfbb298db6c08282afb3d7572b5d6af91954c84d
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-11-10 13:41:54 -0800

    remove brfekaing change + try more

--- apps/svelte-example/tests/capcha.spec.ts
+++ apps/svelte-example/tests/capcha.spec.ts
@@ -3,16 +3,19 @@ import { ai } from "../../../packages/playwright/src/index";
 
 test("solve the captcha", async ({ page }) => {
   await page.goto("/");
-  await page.setViewportSize({
-    width: 1024,
-    height: 768,
-  });
-  await ai(
-    "This is a quiz. Select the image that shows the motorcycle most. then the rest of the motorcycle. click all 4 images that show the motorcycle. then click 'check answer'. if the test is passed the page will say 'Congratulations'. ",
-    {
-      page,
-      test,
+  for (let i = 0; i < 4; i++) {
+    try {
+      await ai(
+        "This is a quiz. Select the image that shows the motorcycle most. then the rest of the motorcycle. click all 4 images that show the motorcycle. then click 'check answer'. if the test is passed the page will say 'Congratulations'. ",
+        {
+          page,
+          test,
+        }
+      );
+      break;
+    } catch (e) {
+      continue;
     }
-  );
+  }
   await page.waitForSelector("text=Congratulations");
 });
--- apps/vite-example/tests/cat-dog.spec.ts
+++ apps/vite-example/tests/cat-dog.spec.ts
@@ -4,10 +4,7 @@ import { ai } from "../../../packages/playwright/src/index";
 test("identify images", async ({ page }) => {
 
   await page.goto("/cat-dog");
-  await page.setViewportSize({
-    width: 1024,
-    height: 768,
-  });
+ 
   await page.waitForLoadState("networkidle");
   await ai("click on the picture of a dog", {
     page,
